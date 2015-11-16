d8620001f908ff7d442ab1d048add9229be8b4e9
Shekhar Gulati <shekhargulati84@gmail.com>
2015-11-16 09:09:16 +0530

    #23 added circle.yml

--- circle.sh
+++ circle.sh
@@ -1,58 +1,6 @@
 #!/bin/bash -ex
 
-case "$1" in
-  pre_machine)
-    # ensure correct level of parallelism
-    expected_nodes=1
-    if [ "$CIRCLE_NODE_TOTAL" -ne "$expected_nodes" ]
-    then
-        echo "Parallelism is set to ${CIRCLE_NODE_TOTAL}x, but we need ${expected_nodes}x."
-        exit 1
-    fi
-
-    # have docker bind to both localhost and unix socket
-    docker_opts='DOCKER_OPTS="$DOCKER_OPTS -D -H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock --registry-mirror=http://localhost:5000"'
-    sudo sh -c "echo '$docker_opts' >> /etc/default/docker"
-
-    cat /etc/default/docker
-
-    ;;
-
-  dependencies)
-    ./gradlew assemble -x signArchives
-
-    docker pull registry
-
-    ;;
-
-  test)
-    set +x
-
-    # expected parallelism: 2x. needs to be set in the project settings via CircleCI's UI.
-    case $CIRCLE_NODE_INDEX in
-      0)
-        # test with http
-        export DOCKER_HOST=tcp://127.0.0.1:2375
-
-        ;;
-
-      1)
-        # test with unix sockets
-        export DOCKER_HOST=unix:///var/run/docker.sock
-
-        ;;
-
-    esac
-
-    ./gradlew clean build -x signArchives
-
-    ;;
-
-  post_test)
-    docker logs registry &> $CIRCLE_ARTIFACTS/registry.log
-
-    cp target/surefire-reports/*.xml $CI_REPORTS
-
-    ;;
-
-esac
+docker_opts='DOCKER_OPTS="$DOCKER_OPTS -D -H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock --registry-mirror=http://localhost:5000"'
+sudo sh -c "echo '$docker_opts' >> /etc/default/docker"
+cat /etc/default/docker
+export DOCKER_HOST=tcp://127.0.0.1:2375
