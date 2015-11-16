59c8462f427fe37318e5492451b6ca654c61b939
Shekhar Gulati <shekhargulati84@gmail.com>
2015-11-16 08:59:41 +0530

    #23 added circle.yml

--- circle.sh
+++ circle.sh
@@ -2,6 +2,14 @@
 
 case "$1" in
   pre_machine)
+    # ensure correct level of parallelism
+    expected_nodes=1
+    if [ "$CIRCLE_NODE_TOTAL" -ne "$expected_nodes" ]
+    then
+        echo "Parallelism is set to ${CIRCLE_NODE_TOTAL}x, but we need ${expected_nodes}x."
+        exit 1
+    fi
+
     # have docker bind to both localhost and unix socket
     docker_opts='DOCKER_OPTS="$DOCKER_OPTS -D -H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock --registry-mirror=http://localhost:5000"'
     sudo sh -c "echo '$docker_opts' >> /etc/default/docker"
@@ -10,9 +18,17 @@ case "$1" in
 
     ;;
 
+  dependencies)
+    ./gradlew assemble -x signArchives
+
+    docker pull registry
+
+    ;;
+
   test)
     set +x
 
+    # expected parallelism: 2x. needs to be set in the project settings via CircleCI's UI.
     case $CIRCLE_NODE_INDEX in
       0)
         # test with http
